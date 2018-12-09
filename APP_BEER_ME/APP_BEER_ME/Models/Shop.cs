dc753e0ff62d26f86e802256031f7e49003a01e0
Tom <tkutela@yahoo.com>
2018-12-09 20:08:01 +0000

    edited shop view

--- APP_BEER_ME/APP_BEER_ME/Models/Shop.cs
+++ APP_BEER_ME/APP_BEER_ME/Models/Shop.cs
@@ -12,12 +12,12 @@ namespace APP_BEER_ME.Models
         [Key]
         public int ShopID { get; set; }
         [Display(Name = "Shop Name")]
-        [Required(ErrorMessage = "The Shop Name field is required ! ")]
         public string ShopName { get; set; }
         [Display(Name = "Shop Address")]
-        [Required(ErrorMessage = "The Shop Address field is required ! ")]
+       [DisplayFormat ()]
         public string ShopAddress { get; set; }
 
         public virtual ICollection<Stock> Stocks { get; set; }
+        
     }
 }
