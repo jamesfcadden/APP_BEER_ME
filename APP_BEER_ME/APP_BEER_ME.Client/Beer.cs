9a2d48d5d9eb20af34e3622ed9d36d6ebcb185fc
Julianna <Julianna@DAIMOS>
2018-12-11 14:32:39 +0000

    Add console client app

---
+++ APP_BEER_ME/APP_BEER_ME.Client/Beer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_BEER_ME.Client
+{
+    public enum Container { Bottle, Can }
+    public class Beer
+    {
+        [Key]
+        public int BeerID { get; set; }
+        [Required(ErrorMessage = "The Name field is required ! ")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The Style field is required ! ")]
+        public string Style { get; set; }
+        [Required(ErrorMessage = "The Brand field is required ! ")]
+        public string Brand { get; set; }
+        [Required(ErrorMessage = "The Brewery field is required ! ")]
+        public string Brewery { get; set; }
+        [Required(ErrorMessage = "The Container field is required ! ")]
+        public Container Container { get; set; }
+        public int Volume { get; set; }
+        [Required(ErrorMessage = "The ABV field is required ! ")]
+        public double ABV { get; set; }
+
+      //  public virtual ICollection<Stock> Stocks { get; set; }
+    }
+}
---
+++ APP_BEER_ME/APP_BEER_ME.Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace APP_BEER_ME.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient myClient = null;
+
+            try
+            {
+                myClient = new HttpClient { BaseAddress = new Uri("https://APPBEERME.azurewebsites.net/") };   // the location on Azure
+                myClient.DefaultRequestHeaders.Accept.Clear();
+                myClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to create HttpClient - program exiting");
+                return;
+            }
+
+
+            GetABeersSortedByBeerNameAsync(myClient).Wait();
+            // GetAllBeersSumAmount(myClient).Wait();
+            Console.ReadLine();
+        }
+        static async Task GetABeersSortedByBeerNameAsync(HttpClient myClient)
+        {
+            try
+            {
+                HttpResponseMessage response = await myClient.GetAsync("api/beers");
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var beers = await response.Content.ReadAsAsync<IEnumerable<Beer>>();
+                    var sorted = beers.OrderByDescending(s => s.Name);
+
+                    Console.WriteLine("\n\t Order List Desc:");
+                    foreach (var c in sorted)
+                    {
+                        Console.WriteLine("\n\t" + c.Name + " \t Sum: " + c.Style + "\t| " + c.Brewery);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(response.StatusCode + " " + response.ReasonPhrase);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
